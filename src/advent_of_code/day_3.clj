3919f0397c1a050ccfe7b4baa189bb192b609b78
chewie <cc.korth@googlemail.com>
2020-12-25 11:40:59 +0100

    Advent of Code - Day 3

--- src/advent_of_code/day_3.clj
+++ src/advent_of_code/day_3.clj
@@ -1,1 +1,19 @@
 (ns advent-of-code.day-3)
+
+(def test-data
+  (clojure.string/split-lines (slurp "src/advent_of_code/day_3.txt")))
+
+(defn day_3-1
+  [test-data x y]
+  (->> (for [i (range)
+             :let [x' (* x i) y' (* y i)]
+             :while (< y' (count test-data))]
+         [y' (mod x' (count (first test-data)))])
+      (filter #(= (get-in test-data %) \#))
+      count))
+
+
+(defn day_3-2 [test-data]
+  (apply * (map #(apply day_3-1 test-data %)
+                [[1 1] [3 1] [5 1] [7 1] [1 2]])))
+
